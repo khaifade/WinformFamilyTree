f8b2e7bd6af510eba0415b8333a4183504352250
uKaxg <uka.pgr@gmail.com>
2023-06-23 12:49:31 +0700

    edit initial home screen

--- FamilyTree.cs
+++ FamilyTree.cs
@@ -77,6 +77,7 @@ namespace WinformFamilyTree
             memberListScreen.Hide();
             sharedScreen.Hide();
             homeScreen.BringToFront();
+            homeScreenButton.PerformClick();
             
         }
         public void AddUserControl(UserControl userControl)
